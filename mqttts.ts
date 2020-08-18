59a3abcb668e44b1b2cab045d54bff6030bd168d
harithzamri <harithtoikee96@gmail.com>
2020-08-18 11:51:14 +0800

    try mqtt with typescript

---
+++ mqttts.ts
@@ -0,0 +1,28 @@
+import mqtt from "mqtt";
+
+var url = "wss://m16.cloudmqtt.com";
+
+const options = {
+  //   host: "wss://m16.cloudmqtt.com",
+  protocolId: "MQTT",
+  port: 31588,
+  username: "jhfsrkth",
+  password: "LeA8c8Q7ZPoH",
+};
+
+var client = mqtt.connect(url, options);
+
+client.on("connect", function (err) {
+  client.publish("test", "hello");
+});
+
+client.on("error", function (err) {
+  console.log(JSON.stringify(err, null, 4));
+  client.end();
+});
+
+client.subscribe("temperature");
+
+client.on("message", function (topic, message, packet) {
+  console.log(message.toString());
+});
